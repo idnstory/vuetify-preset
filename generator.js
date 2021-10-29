90f5d6cda71e241d66fa014585c0c34d29117936
kathar0s <kathar0s.dev@gmail.com>
2021-10-29 14:13:29 +0900

    Spoqa Han Sans Neo 폰트 업데이트

--- generator.js
+++ generator.js
@@ -1,11 +1,21 @@
 const {
   // injectGoogleFontLink,  // 구글 폰트 연결하고 싶을 때 활성화
+  injectHtmlLink,
   generatePreset,
 } = require("@vuetify/cli-plugin-utils");
+
+function injectSpoqaFontLink(api, font) {
+  return injectHtmlLink(
+    api,
+    `https://cdn.haezoom.io/css/SpoqaHanSans${font}.css`,
+    'rel="stylesheet"',
+  );
+}
 
 module.exports = (api) => {
   // 구글 폰트 연결하고 싶을 때
   // 참고: https://vuetifyjs.com/en/features/presets/#generator
-
-  generatePreset(api, "hz");
+  generatePreset(api, "hz", () => {
+    injectSpoqaFontLink(api, "Neo");
+  });
 };
