a32ad145f0c436e3671a2712fe387a33c79285ef
kathar0s <kathar0s.dev@gmail.com>
2021-11-01 16:12:36 +0900

    v1.0.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Imports
-import { injectSassVariables } from "@vuetify/cli-plugin-utils";
+const { injectSassVariables } = require("@vuetify/cli-plugin-utils");
 
-export default (api) =>
+module.exports = (api) =>
   injectSassVariables(
     api,
     "@haezoom/vue-cli-plugin-vuetify-preset/preset/variables.scss",
